4c72a52305096fa0f2b48fe17b8586649dfa9c1f
jmanock <jonmanock@gmail.com>
2019-09-14 12:02:21 -0400

    setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from './Components/Navbar/Header';
+import About from './Components/About';
+import SignUp from './Components/SignUp';
+import Footer from './Components/Navbar/Footer';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <About />
+      <SignUp />
+      <Footer />
     </div>
   );
 }
